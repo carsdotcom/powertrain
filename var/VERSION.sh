3ff2681a239d655ac82741b0eca2a9ce4b5cb9eb
Mac Heller-Ogden <mac@machellerogden.com>
2015-12-15 11:13:37 -0600

    version defaults to git head commit hash if available

--- var/VERSION.sh
+++ var/VERSION.sh
@@ -3,7 +3,12 @@ if [ "$1" == "all" ]; then
     VERSION=
 elif [ -z "$1" ] || [ "$1" == "default" ]; then
     if [ "$VERSION_SCRIPT" == "" ] || [ "$VERSION_SCRIPT" == "default" ]; then
-        VERSION=latest
+         HEAD="$(git rev-parse HEAD 2>&1)"
+         if [[ $HEAD =~ ^[0-9a-f]{5,40}$ ]]; then
+            VERSION=$HEAD
+        else
+            VERSION=latest
+        fi
     else
         VERSION=$($VERSION_SCRIPT)
     fi
