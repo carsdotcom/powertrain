7c4a949e6e9e603fd72a20f5bef65ecafba4593a
Keith Kreissl <kkreissl@cars.com>
2016-01-13 10:56:23 -0600

    PLAT-9999 | added functionality to create the machine if it doesnt exist

--- scripts/machineDoctor.sh
+++ scripts/machineDoctor.sh
@@ -5,6 +5,13 @@ if [[ ! -z "$1" ]]; then
     MACHINE=$1
 fi
 
+docker-machine status $MACHINE
+EXIST=$?
+
+if [[ "$EXIST" == "1" ]]; then
+    $POWERTRAIN_DIR/scripts/machineCreate.sh $MACHINE
+fi
+
 $POWERTRAIN_DIR/scripts/machineRoute.sh
 
 STATUS=`docker-machine status $MACHINE`
