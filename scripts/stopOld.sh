aa3763b471d70af64d10ae73ebafcc58552f643d
Mac Heller-Ogden <mac@machellerogden.com>
2016-01-19 15:16:33 -0600

    PLAT-9999 | better stop-old ... still not perfect

--- scripts/stopOld.sh
+++ scripts/stopOld.sh
@@ -5,8 +5,10 @@ source $POWERTRAIN_DIR/var/NAME.sh ${ARGS[0]}
 source $POWERTRAIN_DIR/var/IMAGE.sh ${ARGS[0]} ${ARGS[1]}
 source $POWERTRAIN_DIR/var/REGISTRY.sh ${ARGS[2]}
 source $POWERTRAIN_DIR/var/INSTANCES.sh ${ARGS[4]}
-CONTAINERS="$(docker ps | grep "$REGISTRY""$NAME" | awk '{print $1}' | xargs docker inspect -f "{{.Created}} {{.Id}}" | sort -r | tail -n +$((INSTANCES + 1)) | awk '{print $2}')"
-if [ -n "$CONTAINERS" ]; then
+TOTAL_INSTANCES="$(docker ps | grep "$REGISTRY""$NAME" | wc -l | awk '{print $1}')"
+OLD_INSTANCES=$((TOTAL_INSTANCES - INSTANCES))
+if [ "$OLD_INSTANCES" -ge 0 ]; then
+    CONTAINERS="$(docker ps | grep "$REGISTRY""$NAME" | awk '{print $1}' | xargs docker inspect -f "{{.Created}} {{.Id}}" | sort -r | tail -n +$((INSTANCES + 1)) | awk '{print $2}')"
     echo "Stopping the following containers:"
     printf "$CONTAINERS"
     printf "$CONTAINERS" | xargs docker stop
