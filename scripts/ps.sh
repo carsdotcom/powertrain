5a1583b22d5e8b20acf47d3f80e61b690fd7437e
Mac Heller-Ogden <mac@machellerogden.com>
2016-02-16 15:29:07 -0600

    updating ps to list all when version is all

--- scripts/ps.sh
+++ scripts/ps.sh
@@ -3,7 +3,11 @@ source $POWERTRAIN_DIR/var/ARGS.sh
 VERSION_SCRIPT=${ARGS[3]}
 source $POWERTRAIN_DIR/var/IMAGE.sh ${ARGS[0]} ${ARGS[1]}
 source $POWERTRAIN_DIR/var/REGISTRY.sh ${ARGS[2]}
-CONTAINERS="$(docker ps | grep $REGISTRY""$IMAGE)"
+if [ "${ARGS[1]}" == "all" ]; then
+    CONTAINERS="$(docker ps -a | grep $REGISTRY""$IMAGE)"
+else
+    CONTAINERS="$(docker ps | grep $REGISTRY""$IMAGE)"
+fi
 if [ -n "$CONTAINERS" ]; then
     echo "Running containers matching \"$REGISTRY""$IMAGE\"..."
     printf "$CONTAINERS"
