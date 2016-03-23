de368f79f4b64a632bd79ba5e5df732cd0edafaf
Mac Heller-Ogden <mac@machellerogden.com>
2016-03-23 16:10:12 -0500

    PLAT-9999 | rm-exited and rmi-dangling tasks ... adding rm-exited as first step in deploy

--- scripts/rmOther.sh
+++ scripts/rmOther.sh
@@ -3,7 +3,7 @@ source $POWERTRAIN_DIR/var/ARGS.sh
 VERSION_SCRIPT=${ARGS[3]}
 source $POWERTRAIN_DIR/var/IMAGE.sh ${ARGS[0]} ${ARGS[1]}
 source $POWERTRAIN_DIR/var/REGISTRY.sh ${ARGS[2]}
-CONTAINERS="$(docker ps -a | tail -n +2 | grep -v $REGISTRY""$IMAGE)"
+CONTAINERS="$(docker ps -a | grep -v $REGISTRY""$IMAGE)"
 if [ -n "$CONTAINERS" ]; then
     echo "Removing the following containers:"
     printf "$CONTAINERS\n"
