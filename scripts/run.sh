52a60e8adf1f25fdb870c193d6da2314eb1b81c6
Mac Heller-Ogden <mac@machellerogden.com>
2016-03-25 09:58:46 -0500

    Revert "PLAT-9999 | default port is optional"
    This reverts commit b8cd8e4ddafe9bedaff7d81bf2bff0aeff5971e3.

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,29 +9,12 @@ source $POWERTRAIN_DIR/var/INSTANCES.sh ${ARGS[3]}
 source $POWERTRAIN_DIR/var/DEFAULT_PORT.sh ${ARGS[4]}
 
 for ((i=1; i<=$INSTANCES; i++)); do
-
-    if [ -n "$DEFAULT_PORT" ]; then
-
-        source $POWERTRAIN_DIR/var/NEXT_PORT.sh $DEFAULT_PORT $NEXT_PORT
-
-        if [ "$RUN_SCRIPT" == "" ] || [ "$RUN_SCRIPT" == "default" ]; then
-            echo "Running default run command..."
-            docker run -d -p $NEXT_PORT:$DEFAULT_PORT $REGISTRY""$IMAGE
-        else
-            echo "Running \"$RUN_SCRIPT\"..."
-            $RUN_SCRIPT $REGISTRY""$IMAGE $NEXT_PORT $DEFAULT_PORT
-        fi
-
+    source $POWERTRAIN_DIR/var/NEXT_PORT.sh $DEFAULT_PORT $NEXT_PORT
+    if [ "$RUN_SCRIPT" == "" ] || [ "$RUN_SCRIPT" == "default" ]; then
+        echo "Running default run command..."
+        docker run -d -p $NEXT_PORT:$DEFAULT_PORT $REGISTRY""$IMAGE
     else
-
-        if [ "$RUN_SCRIPT" == "" ] || [ "$RUN_SCRIPT" == "default" ]; then
-            echo "Running default run command..."
-            docker run -d $REGISTRY""$IMAGE
-        else
-            echo "Running \"$RUN_SCRIPT\"..."
-            $RUN_SCRIPT $REGISTRY""$IMAGE
-        fi
-
+        echo "Running \"$RUN_SCRIPT\"..."
+        $RUN_SCRIPT $REGISTRY""$IMAGE $NEXT_PORT $DEFAULT_PORT
     fi
-
 done
--- var/DEFAULT_PORT.sh
+++ var/DEFAULT_PORT.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [ "$1" == "default" ]; then
-    DEFAULT_PORT=
-elif [ -n "$1" ]; then
-    DEFAULT_PORT=$1
-fi
+DEFAULT_PORT=${1:-1337}
