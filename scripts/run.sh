b8cd8e4ddafe9bedaff7d81bf2bff0aeff5971e3
Mac Heller-Ogden <mac@machellerogden.com>
2016-03-25 09:53:33 -0500

    PLAT-9999 | default port is optional

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,12 +9,29 @@ source $POWERTRAIN_DIR/var/INSTANCES.sh ${ARGS[3]}
 source $POWERTRAIN_DIR/var/DEFAULT_PORT.sh ${ARGS[4]}
 
 for ((i=1; i<=$INSTANCES; i++)); do
-    source $POWERTRAIN_DIR/var/NEXT_PORT.sh $DEFAULT_PORT $NEXT_PORT
-    if [ "$RUN_SCRIPT" == "" ] || [ "$RUN_SCRIPT" == "default" ]; then
-        echo "Running default run command..."
-        docker run -d -p $NEXT_PORT:$DEFAULT_PORT $REGISTRY""$IMAGE
+
+    if [ -n "$DEFAULT_PORT" ]; then
+
+        source $POWERTRAIN_DIR/var/NEXT_PORT.sh $DEFAULT_PORT $NEXT_PORT
+
+        if [ "$RUN_SCRIPT" == "" ] || [ "$RUN_SCRIPT" == "default" ]; then
+            echo "Running default run command..."
+            docker run -d -p $NEXT_PORT:$DEFAULT_PORT $REGISTRY""$IMAGE
+        else
+            echo "Running \"$RUN_SCRIPT\"..."
+            $RUN_SCRIPT $REGISTRY""$IMAGE $NEXT_PORT $DEFAULT_PORT
+        fi
+
     else
-        echo "Running \"$RUN_SCRIPT\"..."
-        $RUN_SCRIPT $REGISTRY""$IMAGE $NEXT_PORT $DEFAULT_PORT
+
+        if [ "$RUN_SCRIPT" == "" ] || [ "$RUN_SCRIPT" == "default" ]; then
+            echo "Running default run command..."
+            docker run -d $REGISTRY""$IMAGE
+        else
+            echo "Running \"$RUN_SCRIPT\"..."
+            $RUN_SCRIPT $REGISTRY""$IMAGE
+        fi
+
     fi
+
 done
