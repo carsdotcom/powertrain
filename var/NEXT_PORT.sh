10c99a5be960916309f04c1e63155e6c241e57ad
Mac Heller-Ogden <mac@machellerogden.com>
2016-05-04 12:40:25 -0500

    PLAT-553 | get ports from actual host instead of just from hostports on running containers

--- var/NEXT_PORT.sh
+++ var/NEXT_PORT.sh
@@ -7,7 +7,22 @@ in_array() {
     return 1
 }
 
-ALLPORTS=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "'$1'/tcp") 0).HostPort}}' $(docker ps -q) 2> /dev/null |  tr -d ' ' | grep -v '^$')
+# if we have netstat and we're on OS X...
+if [ -n "$(which netstat)" ] && [ "$(uname)" == "Darwin" ]; then
+    # if using docker-machine...
+    if [ -n "$(which docker-machine)" ] && [ -n "$DOCKER_MACHINE_NAME" ]; then
+        ALLPORTS=$(docker-machine ssh $DOCKER_MACHINE_NAME netstat -anl | grep -E "^tcp|^udp" | awk '{print $4}' | rev | cut -d ':' -f1 | rev | sort -g | uniq)
+    # if using docker for mac...
+    else
+        ALLPORTS=$(netstat -anl | grep -E "^tcp|^udp" | awk '{print $4}' | rev | cut -d'.' -f1 | rev | sort -g | uniq | grep -v "*")
+    fi
+# if we have netstat and are on Linux...
+elif [ -n "$(which netstat)" ] && [ "$(uname)" == "Linux" ]; then
+    ALLPORTS=$(netstat -anl | grep -E "^tcp|^udp" | awk '{print $4}' | rev | cut -d ':' -f1 | rev | sort -g | uniq)
+# if we're in an unknown OS...
+else
+    ALLPORTS=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "'$1'/tcp") 0).HostPort}}' $(docker ps -q) 2> /dev/null |  tr -d ' ' | grep -v '^$')
+fi
 
 NEXT_PORT=$1
 
