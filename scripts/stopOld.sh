c0906d72a91523fb7d35100fa26d80b283f15c98
Deep Mistry <dmistry@cars.com>
2016-10-12 11:35:12 -0500

    CD-0000 | modified grep for stopping old instances

--- scripts/stopOld.sh
+++ scripts/stopOld.sh
@@ -6,10 +6,10 @@ source $POWERTRAIN_DIR/var/NAME.sh ${ARGS[0]}
 source $POWERTRAIN_DIR/var/IMAGE.sh ${ARGS[0]} ${ARGS[1]}
 source $POWERTRAIN_DIR/var/REGISTRY.sh ${ARGS[2]}
 source $POWERTRAIN_DIR/var/INSTANCES.sh ${ARGS[4]}
-TOTAL_INSTANCES="$(docker ps | grep "$REGISTRY""$NAME" | wc -l | awk '{print $1}')"
+TOTAL_INSTANCES="$(docker ps | grep "$REGISTRY""$NAME": | wc -l | awk '{print $1}')"
 OLD_INSTANCES=$((TOTAL_INSTANCES - INSTANCES))
 if [ "$OLD_INSTANCES" -ge 0 ]; then
-    CONTAINERS="$(docker ps | grep "$REGISTRY""$NAME" | awk '{print $1}' | xargs docker inspect -f "{{.Created}} {{.Id}}" | sort -r | tail -n +$((INSTANCES + 1)))"
+    CONTAINERS="$(docker ps | grep "$REGISTRY""$NAME": | awk '{print $1}' | xargs docker inspect -f "{{.Created}} {{.Id}}" | sort -r | tail -n +$((INSTANCES + 1)))"
     if [ -n "$CONTAINERS" ]; then
         echo "Stopping the following containers:"
         docker stop $(printf "$CONTAINERS" | awk '{print $2}')
